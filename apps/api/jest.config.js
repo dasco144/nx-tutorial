9604adf135c53ef9bbbc20b729246bde3be269f6
Daniel Santiago <daniel.santiago@sybrin.co.za>
2019-08-22 16:47:52 +0200

    Fix path

--- apps/api/jest.config.js
+++ apps/api/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'apps-api',
-  preset: '../../../jest.config.js',
-  coverageDirectory: '../../../coverage/apps/apps/api'
+  name: 'api',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/api'
 };
